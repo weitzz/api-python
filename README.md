b2e77f1ce564b359a396e10be6f8ce062c896835
Tatiane Weitzel <weitzeltati@Gmail.com>
2023-06-06 19:13:55 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Teste API 
+
+## 💻 pharma
+### Para rodar a aplicação
+
+> OBS: pode-se utilizar o gerenciador de pacotes npm ou o yarn para rodar os comandos abaixo
+
+Execute ```npm install``` na pasta dahorta para instalar as dependências.
+
+Rode ```npm start``` na pasta dahorta. Acesse via **localhost:3000**.
+
+---
+
+## :zap: Ferramentas utilizadas:
+- React
+- Typescript
+- Hooks
+- Tailwind
+- Axios
+- React-router-dom v6
+
+![screenshot-localhost_3000-2022 01 23-12_15_30](https://user-images.githubusercontent.com/52081179/150685429-9e6c4ec7-81fb-46b8-8ee4-bfd20f910f32.png)
+
+
+![screenshot-localhost_3000-2022 01 23-11_57_04](https://user-images.githubusercontent.com/52081179/150685270-59e6ed42-4cb1-4356-a32a-c6d8fabf4e84.png)
+
+![screenshot-localhost_3000-2022 01 23-12_04_37](https://user-images.githubusercontent.com/52081179/150685272-d231d7d2-982d-4c41-9745-52b4c8eb8ab8.png)
+
+
+
+
+ ---
+ <p align="center">Feito com ❤️ por <strong>Tatiane Weitzel<p>
--- api/endpoints/medicamentos.py
+++ api/endpoints/medicamentos.py
@@ -28,7 +28,7 @@ async def get_medicamentos(db: AsyncSession = Depends(get_session)):
     async  with db as session:
         query = select(MedicamentoModel)
         result = await  session.execute(query)
-        medicamentos: List[MedicamentoModel] = result.scalars().all()
+        medicamentos = List[MedicamentoModel] = result.scalars().all()
 
         return medicamentos
 
@@ -38,12 +38,24 @@ async def get_medicamento(medicamento_id: int, db: AsyncSession = Depends(get_se
     async with db as session:
         query = select(MedicamentoModel).filter(MedicamentoModel.id == medicamento_id)
         result = await session.execute(query)
-        medicamento= result.scalar_one_or_none()
+        medicamento = result.scalar_one_or_none()
 
         if medicamento:
             return medicamento
         else:
             raise HTTPException(detail='Medicamento não encontrado.', status_code=status.HTTP_404_NOT_FOUND)
+
+
+@router.get('/', response_model=List[MedicamentoSchema])
+async def get_medicamentos(nome: Optional[str] = None, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(MedicamentoModel)
+        if nome:
+            query = query.filter(MedicamentoModel.nome.ilike(f'%{nome}%'))
+        result = await session.execute(query)
+        medicamentos = List[MedicamentoModel] = result.scalars().all()
+
+        return medicamentos
 
 
 @router.put('/{medicamento_id}', response_model=MedicamentoSchema, status_code=status.HTTP_202_ACCEPTED)
@@ -71,7 +83,7 @@ async def delete_medicamento(medicamento_id: int, db: AsyncSession = Depends(get
     async with db as session:
         query = select(MedicamentoModel).filter(MedicamentoModel.id == medicamento_id)
         result = await session.execute(query)
-        medicamento_delete= result.scalar_one_or_none()
+        medicamento_delete = result.scalar_one_or_none()
 
         if medicamento_delete:
             await session.delete(medicamento_delete)
@@ -83,19 +95,21 @@ async def delete_medicamento(medicamento_id: int, db: AsyncSession = Depends(get
 
 
 @router.post('/uploadImage', status_code=status.HTTP_201_CREATED, response_model=MedicamentoSchema)
-async def upload_image(medicamento:MedicamentoSchema, db: AsyncSession = Depends(get_session)):
-
+async def upload_image(medicamento: MedicamentoSchema, db: AsyncSession = Depends(get_session)):
     try:
         db.add(medicamento)
         await db.commit()
         db.refresh()
         return {"message": "Passo 1 concluído com sucesso"}
     except Exception as e:
-        raise HTTPException( detail='Imagem do medicamento não encontrado.', status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        raise HTTPException(detail='Imagem do medicamento não encontrado.',
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @router.get('/getImage', response_model=MedicamentoSchema)
 async def get_image(file: UploadFile = File(...), db: AsyncSession = Depends(get_session)):
-            try:
-                return {"message": "Passo 2 concluído com sucesso"}
-            except Exception as e:
-                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Imagem do medicamento não encontrado.')
+    try:
+        return {"message": "Passo 2 concluído com sucesso"}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail='Imagem do medicamento não encontrado.')
